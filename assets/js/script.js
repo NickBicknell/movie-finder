74af6f7e7852b0814f5b5c0f15ec16b51923e2ee
Nick Bicknell <nicholasbicknell@gmail.com>
2023-07-12 20:40:31 -0500

    added working carousel script to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
-var carousels = bulmaCarousel.attach('.carousel', options);
-
-for(var i = 0; i < carousels.length; i++) {
-	// Add listener to  event
-	carousels[i].on('before:show', state => {
-		console.log(state);
-	});
-}
-
-var element = document.querySelector('#carousel-demo');
-if (element && element.bulmaCarousel) {
-	// bulmaCarousel instance is available as element.bulmaCarousel
-	element.bulmaCarousel.on('before-show', function(state) {
-		console.log(state);
-	});
-}
-
-next();
-
 bulmaCarousel.attach('#carousel-demo', {
     slidesToScroll: 1,
-    slidesToShow: 4
-});
-
-element.bulmaCarousel.on('show', function(bulmaCarouselInstance) {
-    console.log(bulmaCarouselInstance.index);
+    slidesToShow: 2
 });
