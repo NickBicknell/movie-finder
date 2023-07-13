fb2e08e8f9712bdaa66c0606ca5b0f3012aa2a8d
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-07-12 20:21:05 -0500

    added form tags as a container for input tags # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch feature/back-end # Your branch is ahead of 'origin/feature/back-end' by 3 commits. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   assets/js/script.js #	modified:   index.html #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,76 @@
-// todo: set the api keys to variables
+// todo: set api keys
 const firstKey = '';
 const secondKey = '';
+// sets global variables
+let globalResponse = [];
+let globalInputVal = [];
+let globalStorageEl = [];
+let globalStorageVal = [];
+let globalStorageTxt = [];
 
 // todo: set variables with element references
+let inputEl = document.querySelector('.input');
 
 // todo: set fetch functions
 
 // todo: set event listeners
+async function initSearch(searchEvent) {
+	console.log('initSearch()');
+   debugger;
+	searchEvent.preventDefault();
+	// first, get the value of the input text
+	let searchString = inputEl.value.trim();
+	// then, push it into the global array
+	globalInputVal.push(searchString);
+	console.log('globalInputVal:', globalInputVal);
+   debugger;
+	// next, call the fetch function
+	if (searchString) {
+		await fetchActorMovies(searchString);
+		if (globalResponse) {
+			// displays results
+			console.log('globalResponse:', globalResponse);
+		}
+	} else {
+		alert('!searchString');
+	}
+}
+
+// test: this is a test function
+async function fetchActorMovies(searchString) {
+   console.log('fetchActorMovies()');
+   debugger;
+}
 
 // todo: set local storage
+function setLocalStorage() {
+	console.log('setLocalStorage()');
+	let conditionalValue = globalStorageEl.length;
+   debugger;
+	for (let i = 0; i < conditionalValue; i++) {
+		// sets an event listener to the search button
+		globalStorageEl[i].addEventListener('click', function () {
+			// sets the current index to retrieve the text area value
+			let localStorageTxtValue = globalStorageTxt.value;
+			console.log('localStorageTxtValue:', localStorageTxtValue);
+			// sets the value from above into local storage
+			localStorage.setItem(globalStorageEl, localStorageTxtValue);
+		});
+	}
+}
+
+function getLocalStorage() {
+	console.log('getLocalStorage()');
+	let conditionalValue = globalStorageEl.length;
+   debugger;
+	for (let i = 0; i < conditionalValue; i++) {
+		globalStorageTxt[i] = localStorage.getItem(i);
+		globalStorageEl[i].textContent = globalStorageTxt[i];
+	}
+}
 
 // todo: call functions
-window.addEventListener('load', function() {
-   // todo: call functions inside lambda function
-})
+document.addEventListener('load', function () {
+	// todo: call functions inside lambda function
+	setLocalStorage();
+});
